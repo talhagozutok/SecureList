767e554518e0e7f328f79bfbcaeb4e2db165861a
talhagozutok <talhagozutok@users.noreply.github.com>
2024-04-28 11:46:51 +0300

    Add project files.

---
+++ SecureList.Core/Entities/PasswordDirectory.cs
@@ -0,0 +1,24 @@
+﻿namespace SecureList.Core.Entities;
+
+public class PasswordsDirectory : FileSystemInfo
+{
+    public override bool Exists => Directory.Exists(Name);
+    public override string Name => StaticDetails.UnprocessedPasswordsDirectory;
+    public new string FullPath => Name;
+    public override void Delete() => Directory.Delete(Name, recursive: true);
+
+    public IEnumerable<PasswordFile> Files
+    {
+        get
+        {
+            if (!Exists)
+            {
+                Directory.CreateDirectory(Name);
+            }
+
+            return Directory.GetFiles(Name)
+                .Select(file => Path.GetFileName(file))
+                .Select(fileName => new PasswordFile(fileName));
+        }
+    }
+}
---
+++ SecureList.Core/Entities/PasswordFile.cs
@@ -0,0 +1,48 @@
+﻿using SecureList.Core.Helpers;
+using System.Diagnostics;
+
+namespace SecureList.Core.Entities;
+
+public class PasswordFile(string fileName) : FileSystemInfo
+{
+    private readonly PasswordRepository _passwordRepository = PasswordRepository.Instance;
+    private bool _isProcessed = false;
+
+    public override string Name => fileName;
+    public new string FullPath => Path.Join(StaticDetails.UnprocessedPasswordsDirectory, Name);
+    public override bool Exists => File.Exists(FullPath);
+    public override void Delete() => File.Delete(FullPath);
+
+    public void Process()
+    {
+        if (_isProcessed) return;
+
+        Debug.WriteLine($"Info: Processing file {FullPath}");
+        var passwords = FileHelper.ReadLinesFromFile(FullPath);
+        foreach (var pw in passwords)
+        {
+            _passwordRepository.TryAddPassword(pw, Name);
+        }
+
+        Debug.WriteLine($"Success: Processed file {FullPath}");
+        _isProcessed = true;
+        FileHelper.CopyProcessedFile(this);
+
+        return;
+    }
+
+    public bool IsProcessed()
+    {
+        // Compare the contents of the file in Unprocessed-Passwords with the file in Processed.
+        // if contents are not equal return false.
+        var processedPasswordFilePath = Path.Combine(StaticDetails.ProcessedDirectory, fileName);
+        if (!FileHelper.IsFileContentsEqual(FullPath, processedPasswordFilePath))
+        {
+            return false;
+        }
+
+        _isProcessed = false;
+
+        return true;
+    }
+}
