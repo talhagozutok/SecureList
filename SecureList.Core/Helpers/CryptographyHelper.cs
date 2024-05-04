e7c3d5b9365761a923eb5d03382469b67d9e4671
talhagozutok <talhagozutok@users.noreply.github.com>
2024-05-04 11:30:25 +0300

    refactor: clean CalculateHashes() method again

--- SecureList.Core/Helpers/CryptographyHelper.cs
+++ SecureList.Core/Helpers/CryptographyHelper.cs
@@ -5,13 +5,13 @@ namespace SecureList.Core.Helpers;
 
 public static class CryptographyHelper
 {
-    private static readonly SHA256 sha256 = SHA256.Create();
-    private static readonly MD5 md5 = MD5.Create();
-    private static readonly SHA1 sha1 = SHA1.Create();
-
     public static (string md5Hash, string sha1Hash, string sha256Hash) CalculateHashes(string password)
     {
         string md5Hash, sha1Hash, sha256Hash;
+
+        using var md5 = MD5.Create();
+        using var sha1 = SHA1.Create();
+        using var sha256 = SHA256.Create();
 
         md5Hash = CalculateHash(password, md5);
         sha1Hash = CalculateHash(password, sha1);
